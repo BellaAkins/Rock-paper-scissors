d746f523340b4a453751365fee116f6f753ede7a
Sarabella <110487056+BellaAkins@users.noreply.github.com>
2024-10-12 07:18:58 -0700

    Update script.js
    updating the script

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //const check = document.querySelector(".naming");
 //check.innerHTML = yourName;
+/*
 const yourName = prompt("What is your Name?");
+localStorage.setItem("userName", yourName);
+
 const displayyourName = document.querySelector(".name-display");
 
 displayyourName.innerHTML = `Welcome ${yourName}`;
+*/
